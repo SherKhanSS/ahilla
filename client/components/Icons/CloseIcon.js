d889ab49065fc098eca0917595ef1aa97394d7ee
Yuriy <gurovg@gmail.com>
2021-04-05 19:03:33 +0300

    добавляет иконку закрытия

--- client/components/Icons/CloseIcon.js
+++ client/components/Icons/CloseIcon.js
@@ -1,6 +1,25 @@
 const CloseIcon = () => {
   return (
-    'x'
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='20'
+      height='20'
+      fill='none'
+      viewBox='0 0 52 52'
+    >
+      <path
+        fill='#000'
+        fillRule='evenodd'
+        d='M50.087 1.913a3.125 3.125 0 010 4.425l-43.75 43.75a3.129 3.129 0 01-4.425-4.425l43.75-43.75a3.125 3.125 0 014.425 0z'
+        clipRule='evenodd'
+      ></path>
+      <path
+        fill='#000'
+        fillRule='evenodd'
+        d='M1.912 1.913a3.125 3.125 0 000 4.425l43.75 43.75a3.129 3.129 0 004.425-4.425L6.337 1.913a3.125 3.125 0 00-4.425 0z'
+        clipRule='evenodd'
+      ></path>
+    </svg>
   )
 }
 
