e3812de3138167f1210f4c6160da0f11569882a8
Yuriy <gurovg@gmail.com>
2021-09-27 18:46:04 +0300

    добавляет компонент под отображение одной статьи

---
+++ client/components/OnePublication/OnePublication.tsx
@@ -0,0 +1,55 @@
+import { FC } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from './one-publication.module.scss';
+import { formatDate } from '../../utils/utils';
+import { ArticleType } from '../../types';
+import { DangerousHtml } from '../DangerousHtml';
+import Views from '../Icons/Views';
+
+const OnePublication: FC<ArticleType> = ({
+  name,
+  author,
+  image,
+  date,
+  content,
+  views,
+  description,
+}) => {
+  const editContent =
+    description === '' ? content.replace(/\n/g, '</br>') : content;
+
+  return (
+    <article className={styles.main}>
+      <div className={styles.info}>
+        {formatDate(date)}
+        <Link href="/">
+          <a className={styles.link}>{author?.name}</a>
+        </Link>
+      </div>
+      <h3 className={styles.title}>{name}</h3>
+      <div className={styles.img_wrap}>
+        <Image
+          className={styles.image}
+          src={image}
+          alt={name}
+          layout="responsive"
+          width={700}
+          height={400}
+        />
+        <div className={styles.views}>
+          <Views />
+          <span>Просмотры: {views}</span>
+        </div>
+      </div>
+      <div className={styles.wrap}>
+        <div className={styles.text}>
+          {/*<div dangerouslySetInnerHTML={{ __html: content }} />*/}
+          <DangerousHtml str={editContent} />
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default OnePublication;
--- client/pages/[slug].tsx
+++ client/pages/[slug].tsx
@@ -1,13 +1,16 @@
 import { FC } from 'react';
 import { GetServerSideProps } from 'next';
+import Layout from '../components/Layout/Layout';
+import OnePublication from '../components/OnePublication/OnePublication';
+import { ArticleType } from '../types';
 
 const domainURL = process.env.NEXT_PUBLIC_DOMAIN_URL;
 
-const Article: FC<any> = ({ article }) => {
+const Article: FC<{ article: ArticleType }> = ({ article }) => {
   return (
-    <div>
-      {article.name}
-    </div>
+    <Layout>
+      <OnePublication {...article} />
+    </Layout>
   );
 };
 
